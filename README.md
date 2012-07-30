a6290367981d8eadc2fda3b62632e619d8f0447f
Vinicius Massuchetto <viniciusmassuchetto@gmail.com>
2012-07-30 08:53:54 -0400

    File as a script parameter.

---
+++ README.md
@@ -0,0 +1,3 @@
+Formato do Arquivo
+------------------
+
--- pyponto.py
+++ pyponto.py
@@ -14,39 +14,47 @@ shift = timedelta(hours = 8)
 total_extra = timedelta()
 
 parser = OptionParser()
-parser.add_option("-i", "--inicial", dest = "data_inicial",
+parser.add_option("-f", "--file", dest = "date_file",
+    help = u"Arquivo com as datas.")
+parser.add_option("-i", "--init", dest = "date_init",
     help = u"Data inicial no formato DD/MM/YYYY.")
-parser.add_option("-f", "--final", dest = "data_final",
+parser.add_option("-e", "--end", dest = "date_end",
     help = u"Data final no formato DD/MM/YYYY.")
 (options, args) = parser.parse_args()
 
 try:
-    data_inicial = datetime.strptime(options.data_inicial, day_fmt)
-    data_final = datetime.strptime(options.data_final, day_fmt)
+    date_file = options.date_file
+    date_init = datetime.strptime(options.date_init, day_fmt)
+    date_end = datetime.strptime(options.date_end, day_fmt)
 except:
     parser.print_help()
     exit()
 
-pattern = '(?P<data>[0-9/]+)\s+(?P<entrada>[0-9:]+)\s+(?P<saida>[0-9:]+)'
+try:
+    f = open(date_file, 'r')
+except:
+    parser.print_help()
+    exit()
+
+pattern = '(?P<date>[0-9/]+)\s+(?P<init>[0-9:]+)\s+(?P<end>[0-9:]+)'
 pattern = re.compile(pattern)
 
 total_days = 0
 
-f = open('/home/vinicius/doc/ponto.txt', 'r')
 for line in f.readlines():
 
     try:
         m = pattern.match(line)
-        data = datetime.strptime(m.group('data'), day_fmt)
-        entrada = datetime.strptime(m.group('entrada'), time_fmt)
-        saida = datetime.strptime(m.group('saida'), time_fmt)
+        date = datetime.strptime(m.group('date'), day_fmt)
+        init = datetime.strptime(m.group('init'), time_fmt)
+        end = datetime.strptime(m.group('end'), time_fmt)
     except:
         continue
 
-    if data < data_inicial or data > data_final:
+    if date < date_init or date > date_end:
         continue
 
-    extra = saida - entrada - shift - interval
+    extra = end - init - shift - interval
     if extra > timedelta(0):
         status = '+'
         total_extra += extra;
@@ -58,8 +66,8 @@ for line in f.readlines():
     total_days += 1
 
     print '%s\t%s\t%s\t%s%s' % (
-        data.strftime(day_fmt), entrada.strftime(time_fmt),
-        saida.strftime(time_fmt), status, extra)
+        date.strftime(day_fmt), init.strftime(time_fmt),
+        end.strftime(time_fmt), status, extra)
 
 if total_extra.total_seconds() > 0:
     status = '+'
